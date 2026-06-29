900a0d325d37160ab030bd349041b8b706625cbe
Guilherme Moser <guilhermemoserm@gmail.com>
2026-06-29 20:19:53 -0300

    Implementing findById

--- src/authors/repositories/authors-prisma.repository.ts
+++ src/authors/repositories/authors-prisma.repository.ts
@@ -6,6 +6,7 @@ import {
   SearchResult,
 } from '../interfaces/authors.repository';
 import { ICreateAuthor } from '../interfaces/create-author';
+import { NotFoundError } from '@/shared/errors/not-found-error';
 
 export class AuthorsPrismaRepository implements IAuthorsRepository {
   sortableFields: string[] = ['name', 'email', 'createdAt'];
@@ -21,16 +22,29 @@ export class AuthorsPrismaRepository implements IAuthorsRepository {
   delete(id: string): Promise<Author> {
     throw new Error('Method not implemented.');
   }
-  findById(id: string): Promise<Author> {
-    throw new Error('Method not implemented.');
+
+  async findById(id: string): Promise<Author> {
+    return await this.get(id);
   }
+
   findByEmail(email: string): Promise<Author> {
     throw new Error('Method not implemented.');
   }
   search(params: SearchParams): Promise<SearchResult> {
     throw new Error('Method not implemented.');
   }
-  get(id: string): Promise<Author> {
-    throw new Error('Method not implemented.');
+
+  async get(id: string): Promise<Author> {
+    const author = await this.prisma.author.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!author) {
+      throw new NotFoundError(`Author not found using id ${id}`);
+    }
+
+    return author;
   }
 }
