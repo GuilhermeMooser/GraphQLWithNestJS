3ba3cbb49998e6bce0e53ee1b9cb0e1719da8aa1
Guilherme Moser <guilhermemoserm@gmail.com>
2026-07-12 16:38:16 -0300

    feat: Integrate class-validator and class-transformer for input validation

--- src/authors/graphql/inputs/create-author.input.ts
+++ src/authors/graphql/inputs/create-author.input.ts
@@ -1,10 +1,16 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
 
 @InputType()
 export class CreateAuthorInput {
+  @IsString()
+  @IsNotEmpty()
   @Field()
   name: string;
 
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
   @Field()
   email: string;
 }
